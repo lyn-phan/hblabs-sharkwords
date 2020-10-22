e9aea5d04c07fd41dc3622277dc395c9ad8f9c7e
vagrant <vagrant@vagrant.vm>
2020-10-22 05:01:47 +0000

    Added right guesses and wrong guesses

--- server.py
+++ server.py
@@ -9,16 +9,13 @@ app = Flask(__name__)
 def homepage():
     return render_template('index.html')
 
-
 @app.route('/demo')
 def demo():
     return render_template('demo.html')
 
-
 @app.route('/sharkwords')
 def sharkwords():
     return render_template('sharkwords.html')
-
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
